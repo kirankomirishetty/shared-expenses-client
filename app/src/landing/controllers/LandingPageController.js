85ede928f4cd6a91942f2245fa2c73eec13df437
Kiran Komirishetty <kirankomirishetty@yahoo.co.in>
2016-03-23 19:55:44 -0400

    Cod refactored

--- app/src/landing/controllers/LandingPageController.js
+++ app/src/landing/controllers/LandingPageController.js
@@ -4,7 +4,6 @@ controller("LandingPageController", function($scope, $location) {
 	console.log("/LandingController   User name value is ");
 
 	$scope.goToLoginPage = function() {
-		alert("goToLoginPage button clicked...."+$location.url);
 		$location.url("/login");
 	};
 });
